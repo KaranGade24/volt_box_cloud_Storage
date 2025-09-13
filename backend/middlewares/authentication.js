954c3db7fcf4c23bc79a669cce6a6d2184711dca
KaranGade24 <gadekaran24@gmail.com>
2025-09-13 12:10:04 +0000

    change auth funtion

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -3,26 +3,24 @@ import jwt from "jsonwebtoken";
 const authMiddleware = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    console.log(token, req.body);
     if (!token) {
       return res
         .status(401)
         .json({ message: "Access Denied. No token provided." });
     }
 
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!decoded) {
       return res.status(401).redirect("/login");
     }
 
-    // Attach user info to request object
-    req.user = decoded;
+    // Attach user object with id property
+    req.user = { id: decoded.user }; // ✅ now you can use req.user.id
 
-    console.log("Auth Middleware Passed user: ", req.user);
+    console.log("Auth Middleware Passed user:", req.user);
 
-    next(); // Proceed to the next middleware or route
+    next();
   } catch (error) {
     console.error("Auth Middleware Error:", error);
     return res.status(401).redirect("/login");
